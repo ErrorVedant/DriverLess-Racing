3756a32831dce84e77ae062d0c1065519f8d2554
ErrorVedant <121384023+ErrorVedant@users.noreply.github.com>
2023-10-05 22:18:23 +0530

    Add files via upload

--- Driver less meet/Q8.py
+++ Driver less meet/Q8.py
@@ -1,1 +1,37 @@
 # Q)Calculator using Class
+class Calculator:
+    def __init__(self,num1,num2):
+        self.num1=num1
+        self.num2=num2
+    def add(num1,num2):
+        return num1+num2
+    def sub(num1,num2):
+        return num1-num2
+    def mul(num1,num2):
+        return num1*num2
+    def div(num1,num2):
+        return num1/num2
+    
+print("CALCULATOE")
+print("1]add")
+print("2]sub")
+print("3]mul")
+print("4]div")
+a=input("Enter your choice")
+a=int(a)
+
+n1=input("Enter first number")
+n2=input("Enter seconf number")
+n1=int(n1)
+n2=int(n2)
+if (a==1):
+    print(f"Answer is {Calculator.add(n1,n2)}")
+    
+elif (a==2):
+    print(f"Answer is {Calculator.sub(n1,n2)}")
+
+elif (a==3):
+    print(f"Answer is {Calculator.mul(n1,n2)}")
+
+elif (a==4):
+    print(f"Answer is {Calculator.div(n1,n2)}")
